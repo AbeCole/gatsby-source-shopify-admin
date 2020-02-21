7bf6b52c34111c6b63b9a489a57dcb2aaa37b039
AbeCole <me@abecole.com>
2020-02-21 18:55:37 +0000

    somehow missed a file before

--- lib.js
+++ lib.js
@@ -1,35 +1,35 @@
-'use strict';
+"use strict";
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 exports.queryAll = exports.queryOnce = exports.printGraphQLError = undefined;
 
-var _toConsumableArray2 = require('babel-runtime/helpers/toConsumableArray');
+var _toConsumableArray2 = require("babel-runtime/helpers/toConsumableArray");
 
 var _toConsumableArray3 = _interopRequireDefault(_toConsumableArray2);
 
-var _regenerator = require('babel-runtime/regenerator');
+var _regenerator = require("babel-runtime/regenerator");
 
 var _regenerator2 = _interopRequireDefault(_regenerator);
 
-var _promise = require('babel-runtime/core-js/promise');
+var _promise = require("babel-runtime/core-js/promise");
 
 var _promise2 = _interopRequireDefault(_promise);
 
-var _asyncToGenerator2 = require('babel-runtime/helpers/asyncToGenerator');
+var _asyncToGenerator2 = require("babel-runtime/helpers/asyncToGenerator");
 
 var _asyncToGenerator3 = _interopRequireDefault(_asyncToGenerator2);
 
-var _prettyjson = require('prettyjson');
+var _prettyjson = require("prettyjson");
 
 var _prettyjson2 = _interopRequireDefault(_prettyjson);
 
-var _limiter = require('./limiter');
+var _limiter = require("./limiter");
 
 var _limiter2 = _interopRequireDefault(_limiter);
 
-var _fp = require('lodash/fp');
+var _fp = require("lodash/fp");
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -39,11 +39,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
  */
 
 var printGraphQLError = exports.printGraphQLError = function printGraphQLError(e) {
-    var prettyjsonOptions = { keysColor: 'red', dashColor: 'red' };
+  var prettyjsonOptions = { keysColor: "red", dashColor: "red" };
 
-    if (e.response && e.response.errors) console.error(_prettyjson2.default.render(e.response.errors, prettyjsonOptions));
+  if (e.response && e.response.errors) console.error(_prettyjson2.default.render(e.response.errors, prettyjsonOptions));
 
-    if (e.request) console.error(_prettyjson2.default.render(e.request, prettyjsonOptions));
+  if (e.request) console.error(_prettyjson2.default.render(e.request, prettyjsonOptions));
 };
 
 // /**
@@ -58,31 +58,29 @@ var printGraphQLError = exports.printGraphQLError = function printGraphQLError(e
 var limiter = (0, _limiter2.default)();
 
 var queryOnce = exports.queryOnce = function () {
-    var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(allArgs) {
-        var client, query, _allArgs$args, args;
+  var _ref = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee(allArgs) {
+    var client, query, _allArgs$args, args;
 
-        return _regenerator2.default.wrap(function _callee$(_context) {
-            while (1) {
-                switch (_context.prev = _context.next) {
-                    case 0:
-                        client = allArgs.client, query = allArgs.query, _allArgs$args = allArgs.args, args = _allArgs$args === undefined ? { first: 250 } : _allArgs$args;
+    return _regenerator2.default.wrap(function _callee$(_context) {
+      while (1) {
+        switch (_context.prev = _context.next) {
+          case 0:
+            client = allArgs.client, query = allArgs.query, _allArgs$args = allArgs.args, args = _allArgs$args === undefined ? { first: 250 } : _allArgs$args;
+            return _context.abrupt("return", new _promise2.default(function (resolve, reject) {
+              return limiter(allArgs, { resolve: resolve, reject: reject });
+            }));
 
-                        console.log('queryOnce called in shopify-source-admin');
-                        return _context.abrupt('return', new _promise2.default(function (resolve, reject) {
-                            limiter(allArgs, { resolve: resolve, reject: reject });
-                        }));
+          case 2:
+          case "end":
+            return _context.stop();
+        }
+      }
+    }, _callee, undefined);
+  }));
 
-                    case 3:
-                    case 'end':
-                        return _context.stop();
-                }
-            }
-        }, _callee, undefined);
-    }));
-
-    return function queryOnce(_x) {
-        return _ref.apply(this, arguments);
-    };
+  return function queryOnce(_x) {
+    return _ref.apply(this, arguments);
+  };
 }();
 //
 // export const queryOnce = async ({ client, query, args = {first: 250}, attempts = 2 }) => {
@@ -138,54 +136,58 @@ var queryOnce = exports.queryOnce = function () {
  */
 
 var queryAll = exports.queryAll = function () {
-    var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(client, path, query) {
-        var args = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : { first: 250 };
-        var aggregatedResponse = arguments[4];
+  var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(client, path, query) {
+    var args = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : { first: 250 };
+    var aggregatedResponse = arguments[4];
 
-        var _ref3, data, errors, extensions, headers, status, edges, nodes;
+    var _ref3, data, errors, extensions, headers, status, edges, nodes;
 
-        return _regenerator2.default.wrap(function _callee2$(_context2) {
-            while (1) {
-                switch (_context2.prev = _context2.next) {
-                    case 0:
-                        _context2.next = 2;
-                        return queryOnce({ client: client, query: query, args: args });
+    return _regenerator2.default.wrap(function _callee2$(_context2) {
+      while (1) {
+        switch (_context2.prev = _context2.next) {
+          case 0:
+            _context2.next = 2;
+            return queryOnce({
+              client: client,
+              query: query,
+              args: args
+            });
 
-                    case 2:
-                        _ref3 = _context2.sent;
-                        data = _ref3.data;
-                        errors = _ref3.errors;
-                        extensions = _ref3.extensions;
-                        headers = _ref3.headers;
-                        status = _ref3.status;
-                        edges = (0, _fp.get)([].concat((0, _toConsumableArray3.default)(path), ['edges']), data);
-                        nodes = edges.map(function (edge) {
-                            return edge.node;
-                        });
+          case 2:
+            _ref3 = _context2.sent;
+            data = _ref3.data;
+            errors = _ref3.errors;
+            extensions = _ref3.extensions;
+            headers = _ref3.headers;
+            status = _ref3.status;
+            edges = (0, _fp.get)([].concat((0, _toConsumableArray3.default)(path), ["edges"]), data);
+            nodes = edges.map(function (edge) {
+              return edge.node;
+            });
 
 
-                        aggregatedResponse ? aggregatedResponse = aggregatedResponse.concat(nodes) : aggregatedResponse = nodes;
+            aggregatedResponse ? aggregatedResponse = aggregatedResponse.concat(nodes) : aggregatedResponse = nodes;
 
-                        if (!(0, _fp.get)([].concat((0, _toConsumableArray3.default)(path), ['pageInfo', 'hasNextPage']), false, data)) {
-                            _context2.next = 14;
-                            break;
-                        }
-
-                        args.after = (0, _fp.last)(edges).cursor;
-                        return _context2.abrupt('return', queryAll(client, path, query, args, aggregatedResponse));
-
-                    case 14:
-                        return _context2.abrupt('return', aggregatedResponse);
-
-                    case 15:
-                    case 'end':
-                        return _context2.stop();
-                }
+            if (!(0, _fp.get)([].concat((0, _toConsumableArray3.default)(path), ["pageInfo", "hasNextPage"]), false, data)) {
+              _context2.next = 14;
+              break;
             }
-        }, _callee2, undefined);
-    }));
 
-    return function queryAll(_x2, _x3, _x4) {
-        return _ref2.apply(this, arguments);
-    };
+            args.after = (0, _fp.last)(edges).cursor;
+            return _context2.abrupt("return", queryAll(client, path, query, args, aggregatedResponse));
+
+          case 14:
+            return _context2.abrupt("return", aggregatedResponse);
+
+          case 15:
+          case "end":
+            return _context2.stop();
+        }
+      }
+    }, _callee2, undefined);
+  }));
+
+  return function queryAll(_x2, _x3, _x4) {
+    return _ref2.apply(this, arguments);
+  };
 }();

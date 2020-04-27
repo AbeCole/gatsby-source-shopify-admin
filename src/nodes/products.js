73120f4eb3f965e5991aa3ea2120498d793a6b47
AbeCole <me@abecole.com>
2020-04-27 23:59:09 +0100

    test

--- src/nodes/products.js
+++ src/nodes/products.js
@@ -31,25 +31,29 @@ export const createProductNodes = async ({
 
     if (node.metafields && imageMetafields) {
         await Promise.all(
-            imageMetafields.filter(metafieldKey => {
+            imageMetafields.map(async metafieldKey => {
                 const metafield = node.metafields.find(m => m.key === metafieldKey)
                 // this is very basic 'URL validation', something better should be done here
                 // this was added because if you passed a non-valid URL to 'downloadImageAndCreateFileNode'
                 // it would throw an error and stop the build process
-                return metafield && metafield.value.startsWith('http');
-            }).map(async metafieldKey => {
-                const metafield = node.metafields.find(m => m.key === metafieldKey)
-                node[metafieldKey] = {
-                    id: metafield.id,
-                    originalSrc: metafield.value,
-                    localFile___NODE: await downloadImageAndCreateFileNode({
-                        id: metafield.id,
-                        url: metafield.value,
-                        prefix: nodeHelpers.TYPE_PREFIX,
-                        ...imageHelpers
-                    }),
-                };
-            })
+                if (!metafield || !metafield.value.startsWith('http')) return null;
+
+                const fileNodeId = await downloadImageAndCreateFileNode({
+                   id: metafield.id,
+                   url: metafield.value,
+                   prefix: nodeHelpers.TYPE_PREFIX,
+                   ...imageHelpers
+               });
+               if (fileNodeId) {
+                 node[metafieldKey] = {
+                   id: metafield.id,
+                   originalSrc: metafield.value,
+                   localFile___NODE:,
+                 };
+               }
+
+               return null;
+            }).filter(m => m);
         );
     }
 
